d58b3eb48f41e971357f707f22c80f7810b86feb
Haibo Li <haiboli1993@outlook.com>
2022-03-29 15:10:42 +0100

    Update db_script with placeholder values

--- db_script.sql
+++ db_script.sql
@@ -31,7 +31,7 @@ CREATE TABLE User(
     first_name VARCHAR(255) not null,
     email VARCHAR(255) not null,
     password_hash VARCHAR(255) not null,
-    phone_number INTEGER,
+    phone_number VARCHAR(255) not null,
     is_admin boolean not null,
     company_id VARCHAR(255) not null,
     created_at timestamp with time zone,
@@ -57,11 +57,15 @@ CREATE TABLE Orders(
 );
 
 
-
 INSERT INTO Product VALUES (1, "Bronze", 100, 10)
 INSERT INTO Product VALUES (2, "Silver", 200, 15)
 INSERT INTO Product VALUES (3, "Gold", 300, 20)
 INSERT INTO Product VALUES (4, "Platinum", 1000, 1)
 
+INSERT INTO User VALUES (1, "Amrita", "Jyoti", "AJ12024857@gmail.com", "PLACEHOLDER", "07283746573", FALSE, "391380")
 
+
+
+INSERT INTO Orders VALUES (1, 1, TIMESTAMP '2022-03-19 10:23:54+02', 4)
+INSERT INTO Orders VALUES (2, 1, TIMESTAMP '2022-03-20 07:54:42+02', 3)
 
