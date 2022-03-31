d9983331581329f9794ccb16e073bf8d7c981a65
JD-Jord <96055214+JD-Jord@users.noreply.github.com>
2022-03-31 15:01:27 +0100

    Update deleteProduct.ts

--- deleteProduct.ts
+++ deleteProduct.ts
@@ -13,13 +13,15 @@ interface Product {
 }
 
 async function deleteProduct(req: Request, res: Response) {
-    const product: Product[] = (await client.query(/*sql*/'DELETE FROM Products WHERE product_id = $1', [id]), [req.body.product_id]).rows;
+    const product: Product[] = (await client.query(/*sql*/'DELETE FROM Products WHERE product_id = $1', [id], [req.body.product_id])).rows;
     
     const productFound = product[0];
 
     let returnMessage: Message = {
         success: true,
         message: "",
+        
+    if 
 }
 
 deleteProductRoute.post('/deleteProduct', async function (req, res) {
