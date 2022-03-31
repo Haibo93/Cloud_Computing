367d3df931edebf9862dd9ed6f542ea1ba544f55
JD-Jord <96055214+JD-Jord@users.noreply.github.com>
2022-03-31 13:51:47 +0100

    Create deleteProduct.ts

---
+++ deleteProduct.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import { hashing } from './hash';
+import { client, Message } from './main';
+
+export const deleteProduct = express.Router();
+
+interface Product {
+    product_id: number;
+    name: string;
+    cost: number;
+    timeframe: number;
+}
+
+async function deleteProduct(req: Request, res: Response) {
+    await client.query(/*sql*/'DELETE FROM Products WHERE product_id = $1', [id]), [req.body.product_id];
+}
+
+deleteProductRoute.post('/deleteProduct', async function (req, res) {
+    await deleteNewProduct(req, res);
+})
